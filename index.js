011d7377b059f63e24cd94e998e4db81e36afd17
Your Name <freelancermd82@gmail.com>
2024-09-16 19:13:52 +0600

    update patch

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bd0dd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri);
+// console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -86,12 +86,40 @@ async function run() {
 
 
         // user related apis
+
+        app.get('/user', async(req, res) => {
+            const cursor = userCollection.find();
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+
         app.post('/user', async(req, res) => {
             const user = req.body;
             console.log(user);
             const result = await userCollection.insertOne(user);
             res.send(result);
         })
+
+        app.patch('/user', async(req, res) => {
+            const user = req.body;
+            const filter = {email: user.email}
+            const updateDoc = {
+                $set: {
+                    lastLoggedAt: user.lastLoggedAt
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
 
 
